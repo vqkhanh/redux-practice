4252c8fc2f6c479e4dd92c390c82361e33d26be9
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-09-06 08:39:31 +0700

    subscribe

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import store from "./store";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("store change:", store.getState());
+});
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRem/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/code/electron-sandbox/workbench/workbench.htmloved",
+  payload: {
+    id: 1,
+  },
+});
+
+console.log(store.getState());
