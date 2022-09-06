29162ef5ad8df4ff9d8d530dcd3dfdccaf7876f5
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-09-06 07:06:12 +0700

    create reducer

---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+lastId = 0;
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolve: false,
+        },
+      ];
+    case "bugRemoved":
+      return state.filter((x) => x.id !== action.payload.id);
+    default:
+      return state;
+  }
+}
