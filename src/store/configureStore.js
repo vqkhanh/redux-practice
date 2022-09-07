a8d9a8e2ac7610adcf108e211238bdc5b482cf82
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-09-07 12:30:38 +0700

    7.4- Parameterizing Middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,7 +5,7 @@ import reducer from "./reducer";
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger],
+    middleware: [logger({ destination: "console" })],
   });
   return store;
 }
