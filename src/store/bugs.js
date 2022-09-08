434a78cbc679c059157f570ed6119ef4ff4b2198
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-09-08 14:47:03 +0700

    8.6- Restructuring the Store

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -6,23 +6,27 @@ let lastId = 0;
 
 const slice = createSlice({
   name: "bugs",
-  initialState: [],
+  initialState: {
+    list: [],
+    loading: false,
+    lastFetch: null,
+  },
   reducers: {
     // actions  => action handles
     bugAdded: (bugs, action) => {
-      bugs.push({
+      bugs.list.push({
         id: ++lastId,
         description: action.payload.description,
         resolve: false,
       });
     },
     bugResolved: (bugs, action) => {
-      const index = bugs.findIndex((x) => x.id === action.payload.id);
+      const index = bugs.list.findIndex((x) => x.id === action.payload.id);
       bugs[index].resolve = true;
     },
     bugAssignedToUser: (bugs, action) => {
       const { bugId, userId } = action.payload;
-      const index = bugs.findIndex((x) => x.id === bugId);
+      const index = bugs.list.findIndex((x) => x.id === bugId);
       bugs[index].userId = userId;
     },
   },
@@ -33,18 +37,18 @@ export default slice.reducer;
 
 // selector
 // export const getUnresolveBugs = (state) =>
-//   state.entities.bugs.filter((bug) => !bug.resolve);
+//   state.entities.bugs.list.filter((bug) => !bug.resolve);
 
 // memoization
 // bugs => get unresolve bugs from the cache
 export const getUnresolveBugs = createSelector(
   (state) => state.entities.bugs,
   (state) => state.entities.projects,
-  (bugs, projects) => bugs.filter((x) => !x.resolve)
+  (bugs, projects) => bugs.list.filter((x) => !x.resolve)
 );
 
 export const getBugsByUser = (userId) =>
   createSelector(
     (state) => state.entities.bugs,
-    (bugs) => bugs.filter((x) => x.userId === userId)
+    (bugs) => bugs.list.filter((x) => x.userId === userId)
   );
